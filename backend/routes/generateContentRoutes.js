70d7c49709ad089930e5e1278a38e0caa68c4d35
srinivas1405-iiit <117642421+srinivas1405-iiit@users.noreply.github.com>
2024-11-08 02:50:34 +0530

    Deleted llama integrated routes

--- backend/routes/generateContentRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { generateChapters } = require('../controllers/generateContentController');
-const router = express.Router();
-
-router.post('/generate_chapters', generateChapters);
-
-module.exports = router;
